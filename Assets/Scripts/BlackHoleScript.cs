e47604a9c5eabe21ba738967e80095adfd7c2baa
KaptenNeptun <kaptenneptun@gmail.com>
2015-12-12 19:36:25 +0100

    Created BlackHoleScript
    BlackHoles will die and react to collisions with other BlackHoles

---
+++ Assets/Scripts/BlackHoleScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHoleScript : MonoBehaviour {
+	public float radius;
+	public float startRadius = 1.0f;
+	public float startSpeed;
+	public float lifeTime;
+	// Use this for initialization
+	void Start () 
+	{
+		KillBlackhole(lifeTime);
+	}
+	void OnTriggerEnter(Collision2D _collider)
+	{
+		if(_collider.layer == "blackHole")
+		{
+			Debug.Log("Collided with Blackhole nr"+ _collider.gameObject.GetInstanceID());
+			Destroy(this.gameObject);
+		}
+	}
+	void KillBlackhole(float _lifeTime)
+	{
+		if(lifeT<=0)
+		{
+			Debug.Log(this.gameObject.GetInstanceID()+" ran out of time");
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			_lifeTime = _lifeTime - 1*Time.fixedDeltaTime();
+		}
+	}
+}
