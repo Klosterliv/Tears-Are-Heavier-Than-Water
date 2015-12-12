831962ee5e7c8608f72c731e3ac1b89223e7e39f
Klosterliv <nekobebis@gmail.com>
2015-12-13 00:13:02 +0100

    WE are HAPPY

--- Assets/Scripts/BlackHoleScript.cs
+++ Assets/Scripts/BlackHoleScript.cs
@@ -6,29 +6,77 @@ public class BlackHoleScript : MonoBehaviour {
 	public float startRadius = 1.0f;
 	public float startSpeed;
 	public float lifeTime;
+
+	public PointEffector2D gravity;
+	public CircleCollider2D effectorArea;
+	public CircleCollider2D area;
+	public Rigidbody2D rigid;
+
+	bool collided = false;
+
+	public float mass = 1;
 	// Use this for initialization
 	void Start () 
 	{
-		KillBlackhole(lifeTime);
+		//KillBlackhole(lifeTime);
+		//area = (CircleCollider2D) GetComponent(typeof(CircleCollider2D));
+
 	}
 	void OnCollisionEnter2D(Collision2D _collider)
 	{
-		if(_collider.gameObject.layer == 8)
+		if(_collider.collider.CompareTag("BlackHole"))
 		{
 			Debug.Log("Collided with Blackhole nr"+ _collider.gameObject.GetInstanceID());
-			Destroy(this.gameObject);
+
+			BlackHoleScript other = (BlackHoleScript) _collider.collider.GetComponent(typeof(BlackHoleScript));
+			if(collided) return;
+
+			other.collided = true; collided = true;
+			if(other.mass <= mass) {
+				mass += other.mass;
+				//area.radius += other.area.radius;
+				gravity.forceMagnitude = -mass;
+				rigid.mass = mass;
+
+				//effectorArea.radius += other.effectorArea.radius;
+				lifeTime+=1000f;
+
+				transform.localScale = Vector3.one + Vector3.one * mass/10;
+				Destroy(other.gameObject);
+			}
+			else {
+				other.mass += mass;
+				//other.area.radius += area.radius;
+				other.gravity.forceMagnitude = -other.mass;
+				//other.effectorArea.radius += effectorArea.radius;
+				other.rigid.mass = other.mass;
+
+				other.transform.localScale = Vector3.one + Vector3.one * other.mass/10;
+				Destroy(gameObject);
+			}
+
+
+			//Destroy(_collider.collider.gameObject);
+
+
 		}
 	}
-	void KillBlackhole(float _lifeTime)
+	void FixedUpdate() {
+		collided = false;
+	}
+	void Update() {
+		KillBlackhole();
+	}
+	void KillBlackhole()
 	{
-		if(_lifeTime<=0)
+		if(lifeTime<=0)
 		{
 			Debug.Log(this.gameObject.GetInstanceID()+" ran out of time");
 			Destroy(this.gameObject);
 		}
 		else
 		{
-			_lifeTime = _lifeTime - 1*Time.fixedDeltaTime;
+			lifeTime = lifeTime - 1*Time.deltaTime;
 		}
 	}
 }
