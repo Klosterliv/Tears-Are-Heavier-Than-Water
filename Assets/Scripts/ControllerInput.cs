3913d50233a0b1242488c81ef2baec875bc4042d
Klosterliv <nekobebis@gmail.com>
2015-12-12 12:17:45 +0100

    Fackmather

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+
+	public GameObject hand;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 axisInput = new Vector2(0,0);
+
+		axisInput.x = Input.GetAxis("Horizontal");
+		axisInput.y = Input.GetAxis("Vertical");
+
+		hand.transform.position += (Vector3) axisInput*Time.deltaTime;
+	
+	}
+}
